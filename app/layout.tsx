ea3b66fa1b78787b4633bd5912d36f043b91b489
stardust <stardustfxprivate@gmail.com>
2024-04-18 00:43:38 -0700

    change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Sebastian',
+  title: 'Sebastian Ala Torre',
   description: 'My portfolio website',
 }
 
