f68dce412bcffb62d3668faca943368dc45173b3
stardust <stardustfxprivate@gmail.com>
2024-04-21 11:00:46 -0700

    rename NavBar to Socials

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import NavBar from './components/NavBar'
+import Socials from './components/Socials'
 import MainSection from './components/MainSection'
 
 export default function page() {
   return (
     <div className="min-h-screen bg-[#2e3440]">
       <div className="max-w-7xl mx-auto">
-        <NavBar />
+        <Socials />
         <MainSection />
       </div>
     </div>
