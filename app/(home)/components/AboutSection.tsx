3c023bf4c38ae976380d78099dc5f8847f7d458e
stardust <88991140+stardustgd@users.noreply.github.com>
2024-05-15 18:17:25 -0700

    finally added an about section

--- app/(home)/components/AboutSection.tsx
+++ app/(home)/components/AboutSection.tsx
@@ -1,27 +1,13 @@
 export default function AboutSection() {
   return (
     <p className="font-light">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa
-      id neque. Ut lectus arcu bibendum at varius vel pharetra. Nec nam aliquam
-      sem et tortor consequat. Aliquam eleifend mi in nulla posuere sollicitudin
-      aliquam. Dictum non consectetur a erat nam at lectus urna duis. At tellus
-      at urna condimentum mattis pellentesque id nibh tortor. Volutpat diam ut
-      venenatis tellus in metus vulputate eu. Nibh nisl condimentum id venenatis
-      a condimentum vitae sapien. At risus viverra adipiscing at in tellus
-      integer feugiat. Vivamus at augue eget arcu dictum varius duis at
-      consectetur. Ac feugiat sed lectus vestibulum mattis ullamcorper velit.
-      <br /> <br />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Nulla porttitor massa
-      id neque. Ut lectus arcu bibendum at varius vel pharetra. Nec nam aliquam
-      sem et tortor consequat. Aliquam eleifend mi in nulla posuere sollicitudin
-      aliquam. Dictum non consectetur a erat nam at lectus urna duis. At tellus
-      at urna condimentum mattis pellentesque id nibh tortor. Volutpat diam ut
-      venenatis tellus in metus vulputate eu. Nibh nisl condimentum id venenatis
-      a condimentum vitae sapien. At risus viverra adipiscing at in tellus
-      integer feugiat. Vivamus at augue eget arcu dictum varius duis at
-      consectetur. Ac feugiat sed lectus vestibulum mattis ullamcorper velit.
+      Hello! My name is Sebastian Ala Torre! I am a third year Computer Science
+      student at UC Irvine. I am currently learning about Embedded Programming,
+      Network Analysis, and Digital Logic.
+      <br /> <br />I am interested in travelling, nature, cars, minimalism,
+      technology, Linux, and video games. In my free time, I love to learn about
+      new technologies and how I can apply it into my daily life. Feel free to
+      contact me!
     </p>
   )
 }
