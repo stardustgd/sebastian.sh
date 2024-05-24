7c07e6f4f442a90300074347f5e1a4a287387724
stardust <88991140+stardustgd@users.noreply.github.com>
2024-05-24 12:12:33 -0700

    change max-width of GenericCard

--- app/(home)/components/GenericCard.tsx
+++ app/(home)/components/GenericCard.tsx
@@ -14,7 +14,7 @@ export default function GenericCard({
   image,
 }: CardProps) {
   return (
-    <div className="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-transparent bg-clip-border text-[#eceff4] shadow transition-all hover:scale-105 hover:border-t-4 hover:border-[#5e81ac]">
+    <div className="relative flex w-full max-w-[32rem] flex-col rounded-xl bg-transparent bg-clip-border text-[#eceff4] shadow transition-all hover:scale-105 hover:border-t-4 hover:border-[#5e81ac]">
       <div className="relative flex items-center gap-4 pt-0 pb-8 mx-0 mt-4 overflow-hidden text-[#eceff4] bg-transparent shadow-none bg-clip-border">
         <img
           src={`${image}`}
