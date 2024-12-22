0405e37ffad37c0bf6136431b1d680e54ab3e900
stardust <88991140+stardustgd@users.noreply.github.com>
2024-12-21 18:18:12 -0800

    improve responsiveness and add css

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,12 +1,10 @@
 export default function HeroSection() {
   return (
-    <div className="flex flex-col justify-center items-start px-12 xl:px-28 gap-2 xl:gap-4 min-h-screen bg-[#2E3440]">
-      <h1 className="text-[#8FBCBB] text-5xl md:text-8xl font-semibold">
+    <div className="flex flex-col justify-center items-start px-8 md:px-12 xl:px-28 gap-2 xl:gap-4 min-h-screen">
+      <h1 className="text-[#8FBCBB] text-5xl md:text-6xl xl:text-8xl font-semibold">
         Sebastian Ala Torre
       </h1>
-      <h2 className="text-[#D8DEE9] text-2xl md:text-5xl font-medium">
-        CSE @ UCI
-      </h2>
+      <h2 className="text-3xl xl:text-5xl font-medium">CSE @ UCI</h2>
     </div>
   )
 }
