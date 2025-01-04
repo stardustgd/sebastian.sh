4b6f274f0bff5b8ab92875973f704b5d4590c0c5
stardust <88991140+stardustgd@users.noreply.github.com>
2025-01-03 16:34:47 -0800

    add parallax effect

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,13 +1,25 @@
+'use client'
+
 import GridBackground from './GridBackground'
 
+import { motion, useScroll, useTransform } from 'framer-motion'
+
 export default function HeroSection() {
+  const { scrollYProgress } = useScroll({
+    offset: ['start start', 'end start'],
+  })
+
+  const textY = useTransform(scrollYProgress, [0, 1], ['0%', '-900%'])
+
   return (
     <div className="relative flex flex-col justify-center items-start px-8 md:px-12 xl:px-28 gap-2 xl:gap-4 min-h-screen">
       <GridBackground />
-      <h1 className="text-[#8FBCBB] text-5xl md:text-6xl xl:text-8xl font-semibold">
-        Sebastian Ala Torre
-      </h1>
-      <h2 className="text-3xl xl:text-5xl font-medium">CSE @ UCI</h2>
+      <motion.div style={{ y: textY }}>
+        <h1 className="text-[#8FBCBB] text-5xl md:text-6xl xl:text-8xl font-semibold">
+          Sebastian Ala Torre
+        </h1>
+        <h2 className="text-3xl xl:text-5xl font-medium">CSE @ UCI</h2>
+      </motion.div>
     </div>
   )
 }
