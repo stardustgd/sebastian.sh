f06cb00269c13b74935d87bdf6a3c1e350aab1ca
Sebastian Ala Torre <88991140+stardustgd@users.noreply.github.com>
2025-09-01 14:05:56 -0700

    fix: social icon hover (#15)
    * fix: social icon hover
    
    * fix: social icon hover

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,7 +1,28 @@
 import Link from 'next/link'
-import { socials } from '@/constants/consts'
+import { SiGmail, SiLinkedin, SiGithub } from 'react-icons/si'
 
 export default function Contact() {
+  const socials = [
+    {
+      link: 'mailto:sdalator@outlook.com',
+      label: 'Email',
+      icon: SiGmail,
+      className: 'hover:fill-[#bf616a]',
+    },
+    {
+      link: 'https://www.linkedin.com/in/sebastian-at/',
+      label: 'LinkedIn',
+      icon: SiLinkedin,
+      className: 'hover:fill-[#5e81ac]',
+    },
+    {
+      link: 'https://www.github.com/stardustgd',
+      label: 'GitHub',
+      icon: SiGithub,
+      className: 'hover:fill-[#4c566a]',
+    },
+  ]
+
   return (
     <div className="min-h-[50vh] bg-[#21242B] px-4 sm:px-8 py-16">
       <div className="flex flex-col justify-center items-center gap-8">
@@ -20,7 +41,7 @@ export default function Contact() {
                 title={social.label}
               >
                 <Icon
-                  className={`size-12 md:size-16 transition-all hover:scale-125 ${social.color}`}
+                  className={`size-12 md:size-16 transition-all hover:scale-125 ${social.className}`}
                 />
               </Link>
             )
