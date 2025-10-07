74ee51c15842577b0583c2d153ec37900a6aee1e
stardust <88991140+stardustgd@users.noreply.github.com>
2025-10-06 22:41:24 -0700

    chore: reword hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -18,7 +18,7 @@ export default function HeroSection() {
         <h1 className="text-[#8FBCBB] text-5xl md:text-6xl xl:text-8xl font-semibold">
           Sebastian Ala Torre
         </h1>
-        <h2 className="text-3xl xl:text-5xl font-medium">CSE @ UCI</h2>
+        <h2 className="text-3xl xl:text-5xl font-medium">Software Engineer</h2>
       </motion.div>
     </div>
   )
